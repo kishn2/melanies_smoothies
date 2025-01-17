647f68e69336529e21cf1d6a0ea692c70de31529
kishn2 <55523168+kishn2@users.noreply.github.com>
2025-01-17 18:24:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,32 +44,32 @@ if ingredients_list:
     
         st.success('Your Smoothie is ordered!', icon="✅")
 #new section to display
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#import requests
+#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response)
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+#sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 #Convert the Snowpark Dataframe to a Pandas Dataframe so we can use LOC function
-pd_df = my_dataframe.to_pandas()
+#pd_df = my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
-ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:'
-    , my_dataframe
-    , max_selections=5
-    )
+#ingredients_list = st.multiselect(
+#    'Choose up to 5 ingredients:'
+#    , my_dataframe
+#    , max_selections=5
+#    )
 
-if ingredients_list:
-     ingredients_string = ''
+#if ingredients_list:
+#     ingredients_string = ''
+#
+#     for fruit_chosen in   ingredients_list:
+#             ingredients_string +=fruit_chosen + ' '
+#
+#             search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+#             #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
-     for fruit_chosen in   ingredients_list:
-             ingredients_string +=fruit_chosen + ' '
-
-             search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-             #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
-             st.subheader(fruit_chosen + ' Nutrition Information')
-             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-             fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True )
+#             st.subheader(fruit_chosen + ' Nutrition Information')
+#             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+#             fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True )
                 
